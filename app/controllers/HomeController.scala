d0baa25343a4b5f01b911b7ece322e4f5da148b0
matthi <matthi.elbel@gmail.com>
2024-10-09 23:58:00 +0200

    First Url-to-Controller-to-Html Actions addet

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -5,9 +5,14 @@ import com.google.inject.Guice
 import javax.inject.*
 import play.api.*
 import play.api.mvc.*
+import de.htwg.se.set.module
 import de.htwg.se.set.controller
-import de.htwg.se.set.controller.IController
 import de.htwg.se.set.module.SetModule
+import de.htwg.se.set.controller.IController
+import de.htwg.se.set.controller.controller.{ChangePlayerCountAction, Controller, RedoAction, SelectPlayerAction, StartGameAction, SwitchEasyAction, UndoAction}
+import org.apache.pekko.util.ByteString
+import play.api.http.HttpEntity
+//import de.htwg.se.set.module.SetModule
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -16,17 +21,78 @@ import de.htwg.se.set.module.SetModule
 @Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
-  private val injector = Guice.createInjector(new SetModule)
-  private val controller = injector.getInstance(classOf[IController])
+  //private val injector = Guice.createInjector(new SetModule)
+  val injector = Guice.createInjector(new SetModule) //auf das Scala Projekt der Jar wird zugegriffen
+  val controller = injector.getInstance(classOf[IController])
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
   def index(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+    Ok(ansiToHtml(controller.toString)).as("text/html; charset=utf-8")
   }
+
+  def start(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    controller.handleAction(StartGameAction())
+    Ok(ansiToHtml(controller.toString)).as("text/html; charset=utf-8")
+  }
+  def undo(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    controller.handleAction(UndoAction())
+    Ok(ansiToHtml(controller.toString)).as("text/html; charset=utf-8")
+  }
+
+  def redo(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    controller.handleAction(RedoAction())
+    Ok(ansiToHtml(controller.toString)).as("text/html; charset=utf-8")
+  }
+
+  def switchEasy(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    controller.handleAction(SwitchEasyAction())
+    Ok(ansiToHtml(controller.toString)).as("text/html; charset=utf-8")
+  }
+  def changePlayerCount(playerCount: Int): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    controller.handleAction(ChangePlayerCountAction(playerCount) )
+    Ok(ansiToHtml(controller.toString)).as("text/html; charset=utf-8")
+  }
+  def selectPlayer(playerNumber: Int): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    controller.handleAction(SelectPlayerAction(playerNumber) )
+    Ok(ansiToHtml(controller.toString)).as("text/html; charset=utf-8")
+  }
+
+  private def ansiToHtml(text: String): String = {
+    // Ersetze ANSI-Farb- und Formatierungs-Codes durch HTML
+    val html = text
+      // Formatierungen
+      .replaceAll("\u001B\\[1m", "<strong>") // Bold
+      .replaceAll("\u001B\\[4m", "<u>")      // Underline
+      // Farben
+      .replaceAll("\u001B\\[31m", "<span style='color:red;'>")    // Rot
+      .replaceAll("\u001B\\[32m", "<span style='color:green;'>")  // Grün
+      .replaceAll("\u001B\\[33m", "<span style='color:yellow;'>") // Gelb
+      .replaceAll("\u001B\\[34m", "<span style='color:blue;'>")   // Blau
+      .replaceAll("\u001B\\[35m", "<span style='color:purple;'>") // Lila
+      .replaceAll("\u001B\\[36m", "<span style='color:cyan;'>")   // Cyan
+      // Reset
+      .replaceAll("\u001B\\[0m", "</span></strong></u>")
+
+    // Schließe die offenen Tags, um sicherzustellen, dass sie richtig im HTML sind
+    val resetHtml = html
+      .replaceAll("</span></strong></u>", "</strong></u></span>")
+
+    // Zeilenumbrüche
+    val formattedHtml = resetHtml
+      .replaceAll("\n", "<br>")
+
+    // Füge den HTML-Code für die monospaced Schriftart hinzu und setze den Hintergrund auf schwarz
+    s"""
+  <style>
+    body {
+      background-color: black;
+      color: white; /* Weißer Text für bessere Lesbarkeit */
+      font-family: "Courier New", monospace;
+    }
+  </style>
+  <pre>$formattedHtml</pre>
+  """
+  }
+
+
+
 }
--- build.sbt
+++ build.sbt
@@ -1,17 +1,19 @@
 name := """SET Game Web"""
 organization := "pzme"
-
 version := "1.0-SNAPSHOT"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
 scalaVersion := "3.3.1"
 
+// Dependencies
 libraryDependencies += guice
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "7.0.1" % Test
+libraryDependencies += "org.scala-lang.modules" %% "scala-swing" % "3.0.0"
+libraryDependencies += "com.google.inject" % "guice" % "6.0.0"   // Downgrade Guice version
+libraryDependencies += "net.codingwell" %% "scala-guice" % "6.0.0"   // Match Scala-Guice with Guice 6.0.0
+libraryDependencies += "org.playframework" %% "play-guice" % "3.0.5"  // Play Guice integration
 
-// Adds additional packages into Twirl
+// Additional configurations
 //TwirlKeys.templateImports += "pzme.controllers._"
-
-// Adds additional packages into conf/routes
 // play.sbt.routes.RoutesKeys.routesImport += "pzme.binders._"
