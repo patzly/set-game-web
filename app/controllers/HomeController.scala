862c779b5018d2086d8360956dab97dfe0816bcd
Patrick Zedler <mail@patrickzedler.com>
2024-10-21 01:27:52 +0200

    Standardausgabe umleiten als Inhalt

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,18 +1,16 @@
 package controllers
 
 import com.google.inject.Guice
-
-import javax.inject.*
+import de.htwg.se.set.controller.IController
+import de.htwg.se.set.controller.controller.*
+import de.htwg.se.set.{controller, module}
+import de.htwg.se.set.module.SetModule
+import de.htwg.se.set.view.Tui
 import play.api.*
 import play.api.mvc.*
-import de.htwg.se.set.module
-import de.htwg.se.set.controller
-import de.htwg.se.set.module.SetModule
-import de.htwg.se.set.controller.IController
-import de.htwg.se.set.controller.controller.{ChangePlayerCountAction, Controller, RedoAction, SelectPlayerAction, StartGameAction, SwitchEasyAction, UndoAction}
-import org.apache.pekko.util.ByteString
-import play.api.http.HttpEntity
-//import de.htwg.se.set.module.SetModule
+
+import java.io.{ByteArrayOutputStream, PrintStream}
+import javax.inject.*
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -21,78 +19,109 @@ import play.api.http.HttpEntity
 @Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
-  //private val injector = Guice.createInjector(new SetModule)
-  val injector = Guice.createInjector(new SetModule) //auf das Scala Projekt der Jar wird zugegriffen
-  val controller = injector.getInstance(classOf[IController])
+  private val injector = Guice.createInjector(new SetModule)
+  private val controller = injector.getInstance(classOf[IController])
+  private var tui: Tui = _
+
+  private def captureOutput(action: => Unit): String = {
+    val byteArrayStream = new ByteArrayOutputStream()
+    val printStream = new PrintStream(byteArrayStream)
+    val oldOut = System.out
+    synchronized {
+      try {
+        System.setOut(printStream)
+        action
+        System.out.flush()
+        byteArrayStream.toString()
+      } finally {
+        System.setOut(oldOut)
+      }
+    }
+  }
+
+  private def result(action: => Unit) = Ok(views.html.index(ansiToHtml(captureOutput(action))))
 
   def index(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    Ok(ansiToHtml(controller.toString)).as("text/html; charset=utf-8")
+    result {
+      tui = Tui(controller)
+    }
   }
 
-  def start(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    controller.handleAction(StartGameAction())
-    Ok(ansiToHtml(controller.toString)).as("text/html; charset=utf-8")
-  }
-  def undo(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    controller.handleAction(UndoAction())
-    Ok(ansiToHtml(controller.toString)).as("text/html; charset=utf-8")
+  def startGame(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    result {
+      controller.handleAction(StartGameAction())
+    }
   }
 
-  def redo(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    controller.handleAction(RedoAction())
-    Ok(ansiToHtml(controller.toString)).as("text/html; charset=utf-8")
+  def goToPlayerCount(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    result {
+      controller.handleAction(GoToPlayerCountAction())
+    }
   }
 
   def switchEasy(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    controller.handleAction(SwitchEasyAction())
-    Ok(ansiToHtml(controller.toString)).as("text/html; charset=utf-8")
+    result {
+      controller.handleAction(SwitchEasyAction())
+    }
   }
+
   def changePlayerCount(playerCount: Int): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    controller.handleAction(ChangePlayerCountAction(playerCount) )
-    Ok(ansiToHtml(controller.toString)).as("text/html; charset=utf-8")
+    result {
+      controller.handleAction(ChangePlayerCountAction(playerCount))
+    }
   }
-  def selectPlayer(playerNumber: Int): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    controller.handleAction(SelectPlayerAction(playerNumber) )
-    Ok(ansiToHtml(controller.toString)).as("text/html; charset=utf-8")
+
+  def selectPlayer(number: Int): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    result {
+      controller.handleAction(SelectPlayerAction(number))
+    }
+  }
+
+  def addColumn(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    result {
+      controller.handleAction(AddColumnAction())
+    }
+  }
+
+  def selectCards(coordinates: String): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    result {
+      controller.handleAction(SelectCardsAction(coordinates.split("-").toList))
+    }
+  }
+
+  def exit(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    result {
+      controller.handleAction(ExitAction())
+    }
+  }
+
+  def undo(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    result {
+      controller.handleAction(UndoAction())
+    }
+  }
+
+  def redo(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    result {
+      controller.handleAction(RedoAction())
+    }
   }
 
   private def ansiToHtml(text: String): String = {
-    // Ersetze ANSI-Farb- und Formatierungs-Codes durch HTML
     val html = text
       // Formatierungen
       .replaceAll("\u001B\\[1m", "<strong>") // Bold
       .replaceAll("\u001B\\[4m", "<u>")      // Underline
       // Farben
-      .replaceAll("\u001B\\[31m", "<span style='color:red;'>")    // Rot
-      .replaceAll("\u001B\\[32m", "<span style='color:green;'>")  // Grün
-      .replaceAll("\u001B\\[33m", "<span style='color:yellow;'>") // Gelb
-      .replaceAll("\u001B\\[34m", "<span style='color:blue;'>")   // Blau
+      .replaceAll("\u001B\\[31m", "<span style='color: #963832;'>")    // Rot
+      .replaceAll("\u001B\\[32m", "<span style='color: #1f801c;'>")  // Grün
+      .replaceAll("\u001B\\[33m", "<span style='color: #aa8a1e;'>") // Gelb
+      .replaceAll("\u001B\\[34m", "<span style='color: #113dbb;'>")   // Blau
       .replaceAll("\u001B\\[35m", "<span style='color:purple;'>") // Lila
-      .replaceAll("\u001B\\[36m", "<span style='color:cyan;'>")   // Cyan
+      .replaceAll("\u001B\\[36m", "<span style='color: #398789;'>")   // Cyan
       // Reset
       .replaceAll("\u001B\\[0m", "</span></strong></u>")
-
-    // Schließe die offenen Tags, um sicherzustellen, dass sie richtig im HTML sind
-    val resetHtml = html
-      .replaceAll("</span></strong></u>", "</strong></u></span>")
-
-    // Zeilenumbrüche
-    val formattedHtml = resetHtml
-      .replaceAll("\n", "<br>")
-
-    // Füge den HTML-Code für die monospaced Schriftart hinzu und setze den Hintergrund auf schwarz
-    s"""
-  <style>
-    body {
-      background-color: black;
-      color: white; /* Weißer Text für bessere Lesbarkeit */
-      font-family: "Courier New", monospace;
-    }
-  </style>
-  <pre>$formattedHtml</pre>
-  """
+    // Schließe offene Tags, konvertiere Zeilenumbrüche
+    html.replaceAll("</span></strong></u>", "</strong></u></span>").replaceAll("\n", "<br>")
   }
-
-
-
 }
